67f5b0b59075afe4950463e65962827fd584929c
ManojaD2004 <manojadkc2004@gmail.com>
2024-01-15 13:39:17 +0530

    new changes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+const http = require("http").createServer();
 
-const http = require('http').createServer();
-
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://127.0.0.1:3000/",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
-    });
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+  });
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
-
- 
+http.listen(8080, () => console.log("listening on http://localhost:8080"));
